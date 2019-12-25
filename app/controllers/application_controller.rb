299c573a5cf7f1f0f88e336a35ca77e42ba83ade
kantashimizu <gantaiqingshui968@gmail.com>
2019-12-25 14:32:08 +0900

    devise登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name_knj,:family_name_ktn,:name_knj, :name_ktn,:tel])
+  end
+
+  
+    
+
 end
