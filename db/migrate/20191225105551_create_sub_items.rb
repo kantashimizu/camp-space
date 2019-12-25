4cc689c171a65246379642a20f798c2453711360
kantashimizu <gantaiqingshui968@gmail.com>
2019-12-25 19:58:20 +0900

    db編集

--- db/migrate/20191225105551_create_sub_items.rb
+++ db/migrate/20191225105551_create_sub_items.rb
@@ -1,8 +1,9 @@
 class CreateSubItems < ActiveRecord::Migration[5.2]
   def change
     create_table :sub_items do |t|
-      t.string :name, null: false
-      t.string :image, null: false
+      t.string  :name, null: false
+      t.string  :image, null: false
+      t.references :item, null: false, foreign_key: true
       t.timestamps
     end
   end
