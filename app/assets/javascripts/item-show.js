f0f109b438bb766fd7e95c12bb693929ecc6dae2
kantashimizu <gantaiqingshui968@gmail.com>
2019-12-27 16:25:09 +0900

    ビュー２

--- app/assets/javascripts/item-show.js
+++ app/assets/javascripts/item-show.js
@@ -1,5 +1,5 @@
 $(function(){
-  $('.image2').ckick(function(){
+  $('.image2').on("mouseover",function(){
     console.log("なふこ")
     var selectedSrc = $(this).attr('src');
     $('.show-image').html(`<img src="${selectedSrc}" height="500" width="600">`)
