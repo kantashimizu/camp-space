19162e6ca17c4af877c4e7722a576be8b71e4382
kantashimizu <gantaiqingshui968@gmail.com>
2019-12-31 13:40:06 +0900

    決済/バックエンド

--- app/controllers/masters_controller.rb
+++ app/controllers/masters_controller.rb
@@ -11,16 +11,15 @@ class MastersController < ApplicationController
     if @master.save
         @carts.each do |cart| #購入時にカートテーブルの削除
         cart.destroy
-
-        # card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
-        # amount = @master.item.price
-        # Payjp.api_key ='sk_test_a3ad683fae92739356cf5902'
-        # Payjp::Charge.create(
-        # :amount => amount, #支払金額を入力（itemテーブル等に紐づけても良い）
-        # :customer => card.customer_id, #顧客ID
-        # :currency => 'jpy', #日本円
-        # )
       end
+         @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
+         @amount = @master.item.price
+         Payjp.api_key =  'sk_test_15c6311b07c2c5de97aedcb9'
+         Payjp::Charge.create(
+         :amount => @amount, #支払金額を入力（itemテーブル等に紐づけても良い）
+         :customer => @card.customer_id, #顧客ID
+         :currency => 'jpy', #日本円
+         )
     else
       binding.pry
     end
