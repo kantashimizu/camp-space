e0fa140e9bf7bed942e0accfe38fe15eb9e14979
kantashimizu <gantaiqingshui968@gmail.com>
2020-01-01 17:25:12 +0900

    if user_signed_in? 分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,13 +2,17 @@ class ItemsController < ApplicationController
   def index
     @item = Item.all
     @area = Area.all
-    @carts = Cart.where(user_id:current_user.id)
+    if user_signed_in?
+      @carts = Cart.where(user_id:current_user.id)
+    end
   end
 
   def show
     @item = Item.find(params[:id])
     @images= @item.sub_items
-    @carts = Cart.where(user_id:current_user.id)
+    if user_signed_in?
+      @carts = Cart.where(user_id:current_user.id)
+    end
   end
 
 
